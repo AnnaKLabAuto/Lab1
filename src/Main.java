e3463f8ed40b5fd0b339b689b813456711952b09
Ania123678 <149054948+AnnaKLabAuto@users.noreply.github.com>
2023-10-26 19:23:09 +0200

    Java basics homework

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String name = args[0];
+        int age = Integer.parseInt(args[1]);
+        String city = args[2];
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+
+    }
+}
